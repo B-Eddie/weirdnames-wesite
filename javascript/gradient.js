984122eed1ba0b7edb800437678dba37922c4127
CodrOfthefuture <Ilya.asdaghh@gmail.com>
2024-06-12 10:04:09 -0400

    testing

--- javascript/gradient.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('mousemove', (event) => {
-    const x = (window.innerWidth / 2 - event.clientX) / 10;
-    const y = (window.innerHeight / 2 - event.clientY) / 10;
-    const block = document.querySelector('.block');
-    block.style.boxShadow = `${x}px ${y}px 50px 0 rgba(0,0,255,0.5)`;
-});
